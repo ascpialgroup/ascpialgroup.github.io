a4bbb6ffd70fe64892841c6de809b60c80237d9d
ExplodingBottle <72459125+ExplodingBottle@users.noreply.github.com>
2020-11-02 18:16:36 +0100

    Added javascript redirection system

---
+++ js/agredir.js
@@ -0,0 +1,11 @@
+// Obtaining navigator language
+var userLang = navigator.language || navigator.userLanguage;
+
+// Checking language
+if (userLang.startsWith("fr")) {
+    // Redirecting to french page
+    window.location.replace("/fr");
+} else {
+    // Redirecting to english page
+    window.location.replace("/en");
+}
