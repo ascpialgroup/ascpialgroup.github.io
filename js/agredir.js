bec0764d773aff900bf78cd311c5b803b09a30cd
ExplodingBottle <72459125+ExplodingBottle@users.noreply.github.com>
2020-11-07 16:18:23 +0100

    More flexible site root and adding a new page localized. Some minor changes has been added too.

--- js/agredir.js
+++ js/agredir.js
@@ -4,8 +4,8 @@ var userLang = navigator.language || navigator.userLanguage;
 // Checking language
 if (userLang.startsWith("fr")) {
     // Redirecting to french page
-    window.location.replace("/fr");
+    window.location.replace("fr");
 } else {
     // Redirecting to english page
-    window.location.replace("/en");
+    window.location.replace("en");
 }
